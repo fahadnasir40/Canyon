3047e88a38867f9ec1fc12076749e155165b07a9
AsadSiddiqui1990 <siddiquica2007@gmail.com>
2020-08-27 16:56:23 +0500

    commit

--- client/src/components/Purchases/itemsreturn.js
+++ client/src/components/Purchases/itemsreturn.js
@@ -17,17 +17,26 @@ class Itemsreturn extends Component {
                             </select>
                         </div>
                     </div>
-                    <div class="tb-tnx-date">
-                        <span class="date">10-05-2019</span>
-                        <span class="date">10-13-2019</span>
+                    <div class="tb-tnx-desc">
+                        <div class="form-group">
+                            <div class="form-control-wrap">
+                                <input type="text" class="form-control" id="quantity" placeholder="quantity" />
+                            </div>
+                        </div>
+                    </div>
+                </td>
+                <td class="tb-tnx-info">
+                    <div class="tb-tnx-desc">
+                        <div class="form-group">
+                            <div class="form-control-wrap">
+                                <input type="text" class="form-control" id="rate" placeholder="rate" disabled value = "120"/>
+                            </div>
+                        </div>
                     </div>
                 </td>
                 <td class="tb-tnx-amount is-alt">
                     <div class="tb-tnx-total">
-                        <span class="amount">$599.00</span>
-                    </div>
-                    <div class="tb-tnx-status">
-                        <span class="badge badge-dot badge-warning">Due</span>
+                        <span class="amount">1440.00</span>
                     </div>
                 </td>
                 <td class="tb-tnx-action">
@@ -35,9 +44,9 @@ class Itemsreturn extends Component {
                         <a class="text-soft dropdown-toggle btn btn-icon btn-trigger" data-toggle="dropdown"><em class="icon ni ni-more-h"></em></a>
                         <div class="dropdown-menu dropdown-menu-right dropdown-menu-xs">
                             <ul class="link-list-plain">
-                                <li><a href="#">View</a></li>
-                                <li><a href="#">Edit</a></li>
-                                <li><a onClick={()=>{console.log("Remove Called",this.props.index); this.props.remove(this.props.item.key)}}>Remove</a></li>
+                                {/* <li><a href="#">View</a></li> */}
+                                {/* <li><a href="#">Edit</a></li> */}
+                                <li><a onClick={() => { console.log("Remove Called", this.props.index); this.props.remove(this.props.item.key) }}>Remove</a></li>
                             </ul>
                         </div>
                     </div>
